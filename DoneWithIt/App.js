88b49dfa840fac91520019601b0308fb77e979a7
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2023-05-04 12:09:03 -0500

    moved files to github repo

---
+++ DoneWithIt/App.js
@@ -0,0 +1,39 @@
+
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+   <View style={{
+    backgroundColor: '#fff',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+   }}>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: 100,
+        height: 100,
+      }}/>
+      <View style={{
+        backgroundColor: 'gold',
+        width: 100,
+        height: 100,
+      }}/>
+      <View style={{
+        backgroundColor: 'tomato',
+        width: 100,
+        height: 100,
+      }}/>
+   </View>
+  );  
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',  
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
